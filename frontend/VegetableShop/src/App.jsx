461d9e5bdefb5f56a11445d1ffe9f3e1daa323ae
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2025-03-22 20:15:47 +0700

    First Setup VegetableShop

---
+++ frontend/VegetableShop/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Routes, Route } from 'react-router'
+import {HomePage} from './pages/HomePage'
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<HomePage />}></Route>
+    </Routes>
+  )
+}
+
+export default App
