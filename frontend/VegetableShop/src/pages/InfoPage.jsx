6f0da8e9e30f6651a16e86ced02b665049711451
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-03-25 01:40:48 +0700

    frontend Infopage

--- frontend/VegetableShop/src/pages/InfoPage.jsx
+++ frontend/VegetableShop/src/pages/InfoPage.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import styles from '../style/InfoPage.module.css'
 
 function InfoPage() {
   return (
     <div>
       <Header />       
-      <main className="home-content">
-        <h1>Welcome home</h1>
-      </main>
+        <div>
+            <div className={styles.AboutUs}>
+                <div className={styles.Text}>
+                    <div className={styles.BoldText}>100% Trusted Organic Food Store</div>
+                    <div className={styles.SmallText}>Morbi porttitor ligula in nunc varius sagittis. Proin dui nisi, laoreet ut tempor ac, cursus vitae eros. Cras quis ultricies elit. Proin ac lectus arcu. Maecenas aliquet vel tellus at accumsan. Donec a eros non massa vulputate ornare. Vivamus ornare commodo ante, at commodo felis congue vitae. </div>
+                </div>
+                <div className={styles.Image}>
+                    <img src="/img/About-us-img.png" alt="img" />
+                </div>
+            </div>
+            <div className={styles.AboutMe}>
+                <img src="/img/About-me-img.png" alt="img" />
+            </div>
+        </div>
       <Footer /> 
     </div>
   )
