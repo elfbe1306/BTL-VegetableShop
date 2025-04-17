9c5e6481d87e3c8a8b59c2c578bb4a3acbf811b3
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2025-04-17 17:33:12 +0700

    responsive desgin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    allowedHosts: ['79e7-113-161-40-158.ngrok-free.app'],
+  },
 })
