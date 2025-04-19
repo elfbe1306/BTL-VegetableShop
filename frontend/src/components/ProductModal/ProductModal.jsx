b9136f7ba03f4a874d6de7eb5fec04fdf24e1260
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2025-04-20 01:49:21 +0700

    responsive

--- frontend/src/components/ProductModal/ProductModal.jsx
+++ frontend/src/components/ProductModal/ProductModal.jsx
@@ -1,23 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ProductModal.module.css";
+import CloseIcon from "../../assets/icons/CloseIcon";
 
 const ProductModal = ({ product, onClose }) => {
-  return (
-    <div className={styles.overlay}>
-      <div className={styles.modal}>
-        <div className={styles.productContainer}>
-            <div>
-                
-            </div>
-            <div>
-                <h2>{product.name}</h2>
-                <p>Price: ${product.price}</p>
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const slides = [
+        {
+            image: product.image + "1.png",
+        },
+        {
+            image: product.image + "2.png",
+        },
+        {
+            image: product.image + "3.png",
+        },
+    ];
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+        setCurrentSlide((prev) => (prev + 1) % slides.length);
+        }, 3000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        <div className={styles.overlay}>
+            <div className={styles.modal}>
+                <div className={styles.productContainer}>
+                    <div className={styles.sliderContainer}>
+                        <img src={slides[currentSlide].image}/>
+                        
+                        <div className={styles.dots}>
+                            {slides.map((_, index) => (
+                            <span
+                                key={index}
+                                className={index === currentSlide ? styles.active : ""}
+                                onClick={() => setCurrentSlide(index)}
+                            />
+                            ))}
+                        </div>
+                    </div>
+                    <div>
+                        <h2>{product.name}</h2>
+                        <p>Price: ${product.price}</p>
+                    </div>
+                </div>
+                <button className={styles.closeButton} onClick={onClose}>
+                    <CloseIcon/>
+                </button>
             </div>
         </div>
-        <button onClick={onClose}>Close</button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductModal;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['79e7-113-161-40-158.ngrok-free.app'],
+    allowedHosts: ['https://6482-42-116-54-171.ngrok-free.app'],
   },
 })
