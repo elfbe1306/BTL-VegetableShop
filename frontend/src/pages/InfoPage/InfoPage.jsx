6e7ba7c3b0e384525350ae4cde9a399f2a473e48
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-04-20 18:31:12 +0700

    fix buf in InfoPage

--- frontend/src/pages/InfoPage/InfoPage.jsx
+++ frontend/src/pages/InfoPage/InfoPage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import styles from './InfoPage.module.css'
-import { FaLeaf } from "react-icons/fa";
+import { PiLeafLight } from "react-icons/pi";
+import { CiStar } from "react-icons/ci";
+import { PiTruckLight } from "react-icons/pi";
+import { TfiHeadphoneAlt } from "react-icons/tfi";
+import { PiShoppingBagLight } from "react-icons/pi";
+import { PiPackageLight } from "react-icons/pi";
 
 function InfoPage() {
   return (
@@ -28,18 +33,61 @@ function InfoPage() {
                     <div className={styles.SmallText}>Pellentesque a ante vulputate leo porttitor luctus sed eget eros. Nulla et rhoncus neque. Duis non diam eget est luctus tincidunt a a mi. Nulla eu eros consequat tortor tincidunt feugiat. </div>
                 </div>
                 <div className={styles.FullIconWrap}>
-                    <div className={styles.IAndT}>
-                      <div className={styles.icon}><FaLeaf/></div>
-                      <div className={styles.IconText}>
-                        <div className={styles.IconHeadText}>100% Organic food</div>
-                        <div className={styles.IconContentText}>100% healthy & Fresh food.</div>
+                    <div className={styles.FirstCol}>
+                      <div className={styles.IAndT}>
+                        <div className={styles.icon}><PiLeafLight /></div>
+                        <div className={styles.IconText}>
+                          <div className={styles.IconHeadText}>100% Organic food</div>
+                          <div className={styles.IconContentText}>100% healthy & Fresh food.</div>
+                        </div>
+                      </div>
+                      <div className={styles.IAndT}>
+                        <div className={styles.icon}><CiStar /></div>
+                        <div className={styles.IconText}>
+                          <div className={styles.IconHeadText}>Customer Feedback</div>
+                          <div className={styles.IconContentText}>Our happy customer</div>
+                        </div>
+                      </div>
+                      <div className={styles.IAndT}>
+                        <div className={styles.icon}><PiTruckLight /></div>
+                        <div className={styles.IconText}>
+                          <div className={styles.IconHeadText}>Free Shipping</div>
+                          <div className={styles.IconContentText}>Free shipping with discount</div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className={styles.SeCol}>
+                      <div className={styles.IAndT}>
+                        <div className={styles.icon}><TfiHeadphoneAlt /></div>
+                        <div className={styles.IconText}>
+                          <div className={styles.IconHeadText}>Great Support 24/7</div>
+                          <div className={styles.IconContentText}>Instant access to Contact</div>
+                        </div>
+                      </div>
+                      <div className={styles.IAndT}>
+                        <div className={styles.icon}><PiShoppingBagLight /></div>
+                        <div className={styles.IconText}>
+                          <div className={styles.IconHeadText}>100% Sucure Payment</div>
+                          <div className={styles.IconContentText}>We ensure your money is save</div>
+                        </div>
+                      </div>
+                      <div className={styles.IAndT}>
+                        <div className={styles.icon}><PiPackageLight /></div>
+                        <div className={styles.IconText}>
+                          <div className={styles.IconHeadText}>100% Organic Food</div>
+                          <div className={styles.IconContentText}>100% healthy & Fresh food.</div>
+                        </div>
                       </div>
                     </div>
                 </div>
               </div>
-
-
             </div>
+            <div className={styles.Delivery}>
+                  <div className={styles.Image}>
+                      <img src="src/assets/images/About-us-img.png" alt="img" />
+                  </div>
+              </div>
         </div>
       <Footer /> 
     </div>
