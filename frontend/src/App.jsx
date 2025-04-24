612bf91ef3d7c1f38e1329e7d58460ccb780232b
jun28th <doanlevy2806@gmail.com>
2025-04-25 02:38:32 +0700

    page ContactUs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import 'remixicon/fonts/remixicon.css'
 import HomePage from './pages/HomePage/HomePage'
 import ListProduct from './pages/ListProduct/ListProduct'
 import ProductDetail from './pages/ProductDetail/ProductDetail'
+import ContactPage from './pages/ContactPage/ContactPage'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route path="/vegetable" element={<ListProduct/>} />
         <Route path="/vegetable/:productName" element={<ProductDetail />} />
         <Route path="/blog" element={<div>Welcome to the Blog Page</div>} />
-        <Route path="/contact" element={<div>Contact Us Here</div>} />
+        <Route path="/contact" element={<ContactPage />} />
         <Route path="/about" element={<div>Contact Us Here</div>} />
         <Route path="/faqs" element={<div>Contact Us Here</div>} />
         <Route path="/shoppingcart" element={<div>Contact Us Here</div>} />
