6aa4f423e7cba23ecc07182fb823b363a3a6c467
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-04-26 13:01:47 +0700

    uppdate Our Team

--- frontend/src/components/OurTeam/OurTeam.jsx
+++ frontend/src/components/OurTeam/OurTeam.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./OurTeam.css";
+import styles from "./OurTeam.module.css";
 
 const OurTeam = () => {
     return (
       <div>
-        <div className="ImageBlockWrap">
-            <div className="ImageBlock">
+        <div className={styles.ImageBlockWrap}>
+            <div className={styles.ImageBlock}>
             <img src="src/assets/images/ImageSample1.png" alt="img" />
-                <div  className="overlay">
+                <div  className={styles.overlay}>
                     <div className="social-icons">
                         <a href="#" className="social-icon facebook"><i className="fab fa-facebook-f"></i></a>
                         <a href="#" className="social-icon twitter"><i className="fab fa-twitter"></i></a>
@@ -15,15 +15,15 @@ const OurTeam = () => {
                         <a href="#" className="social-icon instagram"><i className="fab fa-instagram"></i></a>
                     </div>
                 </div>
-                <div className="BlockText">
-                    <div className="HeadText">Jenny Wilson</div>
-                    <div className="SmallText">Ceo & Founder</div>
+                <div className={styles.BlockText}>
+                    <div className={styles.HeadText}>Jenny Wilson</div>
+                    <div className={styles.SmallText}>Ceo & Founder</div>
                 </div>
             </div>
             
-            <div className="ImageBlock">
+            <div className={styles.ImageBlock}>
                 <img src="src/assets/images/ImageSample2.png" alt="img" />
-                <div  className="overlay">
+                <div  className={styles.overlay}>
                     <div className="social-icons">
                         <a href="#" className="social-icon facebook"><i className="fab fa-facebook-f"></i></a>
                         <a href="#" className="social-icon twitter"><i className="fab fa-twitter"></i></a>
@@ -31,15 +31,15 @@ const OurTeam = () => {
                         <a href="#" className="social-icon instagram"><i className="fab fa-instagram"></i></a>
                     </div>
                 </div>
-                <div className="BlockText">
-                    <div className="HeadText">Jane Cooper</div>
-                    <div className="SmallText">Worker</div>
+                <div className={styles.BlockText}>
+                    <div className={styles.HeadText}>Jane Cooper</div>
+                    <div className={styles.SmallText}>Worker</div>
                 </div>
             </div>
 
-            <div className="ImageBlock">
+            <div className={styles.ImageBlock}>
                 <img src="src/assets/images/ImageSample3.png" alt="img" />
-                <div  className="overlay">
+                <div  className={styles.overlay}>
                     <div className="social-icons">
                         <a href="#" className="social-icon facebook"><i className="fab fa-facebook-f"></i></a>
                         <a href="#" className="social-icon twitter"><i className="fab fa-twitter"></i></a>
@@ -47,15 +47,15 @@ const OurTeam = () => {
                         <a href="#" className="social-icon instagram"><i className="fab fa-instagram"></i></a>
                     </div>
                 </div>
-                <div className="BlockText">
-                    <div className="HeadText">Cody Fisher</div>
-                    <div className="SmallText">Security Guard</div>
+                <div className={styles.BlockText}>
+                    <div className={styles.HeadText}>Cody Fisher</div>
+                    <div className={styles.SmallText}>Security Guard</div>
                 </div>
             </div>
 
-            <div className="ImageBlock">
+            <div className={styles.ImageBlock}>
             <img src="src/assets/images/ImageSample4.png" alt="img" />
-                <div  className="overlay">
+                <div  className={styles.overlay}>
                     <div className="social-icons">
                         <a href="#" className="social-icon facebook"><i className="fab fa-facebook-f"></i></a>
                         <a href="#" className="social-icon twitter"><i className="fab fa-twitter"></i></a>
@@ -63,9 +63,9 @@ const OurTeam = () => {
                         <a href="#" className="social-icon instagram"><i className="fab fa-instagram"></i></a>
                     </div>
                 </div>
-                <div className="BlockText">
-                    <div className="HeadText">Robert Fox</div>
-                    <div className="SmallText">Senior Farmer Manager</div>
+                <div className={styles.BlockText}>
+                    <div className={styles.HeadText}>Robert Fox</div>
+                    <div className={styles.SmallText}>Senior Farmer Manager</div>
                 </div>
             </div>
         </div>
