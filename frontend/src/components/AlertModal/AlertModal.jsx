0b981fa51e2e4b9dce449ca264e2b721a41a3fe1
jun28th <doanlevy2806@gmail.com>
2025-04-30 00:52:39 +0700

    add modal for checkout

--- frontend/src/components/AlertModal/AlertModal.jsx
+++ frontend/src/components/AlertModal/AlertModal.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import styles from './AlertModal.module.css';
 import CloseIcon from "../../assets/icons/CloseIcon";
+import { useNavigate } from "react-router-dom";
+import Check from "../../assets/icons/Check"
 
-const AlertModal = ({ message, onClose }) => {
-  return(
+const AlertModal = ({ message, onClose, redirectPath, redirectText }) => {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate(redirectPath);
+    onClose(null);
+  };
+
+  return (
     <div className={styles.overlay}>
       <div className={styles.AlertModalContainer}>
-        <p>{message}</p>
-
-        <button className={styles.closeButton} onClick={() => onClose(false)}>
+        <p>
+          {message}
+          {message === "Order placed successfully!" && (
+            <Check className={styles.CheckIcon} />
+          )}
+        </p>
+        {redirectPath && (
+          <div className={styles.buttonWrapper}>
+            <button className={styles.redirectButton} onClick={handleRedirect}>
+              {redirectText}
+            </button>
+          </div>
+        )}
+        <button className={styles.closeButton} onClick={() => onClose(null)}>
           <CloseIcon />
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AlertModal;
