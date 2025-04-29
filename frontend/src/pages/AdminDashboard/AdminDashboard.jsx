18e7997d0c7125c8351225be1192a21ecbb1bae9
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-04-29 14:38:44 +0700

    Update Admin Dashboard

--- frontend/src/pages/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,14 +1,16 @@
-import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
+import { BarChart2, ShoppingBag, Users, Zap, MessageSquareText } from "lucide-react";
 import { motion } from "framer-motion";
 import styles from './AdminDashboard.module.css'
 
 import Header from "../../components/Admin/Header/Header";
 import StatCard from "../../components/Admin/StatCard/StatCard";
 import SalesOverviewChart from "../../components/Admin/SalesOverviewChart/SalesOverviewChart";
+import SideBar from "../../components/Admin/SideBar/SideBar";
 
 const AdminDashboard = () => {
 	return (
     <div className={styles.BigWrapper}>
+      <SideBar/>
       <div className={styles.Wrapper}>
 			<Header title='Overview' />
 
@@ -22,6 +24,7 @@ const AdminDashboard = () => {
             <StatCard name="Total Sales" icon={Zap} value="$12,345" color="#6366F1" />
             <StatCard name="New Users" icon={Users} value="1,234" color="#8B5CF6" />
             <StatCard name="Total Products" icon={ShoppingBag} value="567" color="#EC4899" />
+            <StatCard name="Total Review" icon={ MessageSquareText} value="300" color="#10B981" />
           </motion.div>
 
 
