c397ed6fbfa68efafeee4c44d43b0a0f99577d4d
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-05-01 15:25:26 +0700

    Update Sidebar.jsx

--- frontend/src/components/Admin/SideBar/SideBar.jsx
+++ frontend/src/components/Admin/SideBar/SideBar.jsx
@@ -16,7 +16,7 @@ const SIDEBAR_ITEMS = [
 ];
 
 const Sidebar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <motion.div
