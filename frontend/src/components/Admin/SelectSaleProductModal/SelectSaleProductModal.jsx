46d4f43b26f3acb550a62181494172d171008b39
SongKhuee <148840532+SongKhuee@users.noreply.github.com>
2025-05-05 14:41:42 +0700

    Revert "overflow san pham"
    This reverts commit 94917f11776242adbe5594c31d6f236b735e71ae.

--- frontend/src/components/Admin/SelectSaleProductModal/SelectSaleProductModal.jsx
+++ frontend/src/components/Admin/SelectSaleProductModal/SelectSaleProductModal.jsx
@@ -21,43 +21,41 @@ const SelectSaleProductModal = ({ onClose, products, selectedProducts, setSelect
   return(
     <div className={styles.overlay}>
       <div className={styles.modal}>
-        <div className={styles.tableContainer}>
-          <table className={styles.table}>
-            <thead>
-              <tr>
-                  {["Name", "Price", "Quantity", "Action"].map((head) => (
-                  <th key={head} className={styles.th}>
-                      {head}
-                  </th>
-                  ))}
-              </tr>
-            </thead>
-            <tbody>
-              {products.map((product) => {
-                return(
-                  <motion.tr
-                    key={product.product_id}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.3 }}
-                  > 
-                    <td className={styles.td}>{product.product_name}</td>
-                    <td className={styles.td}>${product.product_price}</td>
-                    <td className={styles.td}>{product.quantity}</td>
-                    <td>
-                      <input
-                        type="checkbox"
-                        checked={isSelected(product.product_id)}
-                        onChange={() => handleToggle(product)}
-                        className={styles.checkBox}
-                      />
-                    </td>
-                  </motion.tr>
-                )
-              })}
-            </tbody>
-          </table>
-        </div>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+                {["Name", "Price", "Quantity", "Action"].map((head) => (
+                <th key={head} className={styles.th}>
+                    {head}
+                </th>
+                ))}
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => {
+              return(
+                <motion.tr
+                  key={product.product_id}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.3 }}
+                > 
+                  <td className={styles.td}>{product.product_name}</td>
+                  <td className={styles.td}>${product.product_price}</td>
+                  <td className={styles.td}>{product.quantity}</td>
+                  <td>
+                    <input
+                      type="checkbox"
+                      checked={isSelected(product.product_id)}
+                      onChange={() => handleToggle(product)}
+                      className={styles.checkBox}
+                    />
+                  </td>
+                </motion.tr>
+              )
+            })}
+          </tbody>
+        </table>
         <button className={styles.closeButton} onClick={onClose}>
           <CloseIcon />
         </button>
